f88cc1eeabfbb086a759bf127472c3dd1b8c5947
smichon <sylwester.michon@gmail.com>
2018-04-08 00:00:15 +0200

    mod 18.4 TrelloClientTest added

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -20,8 +20,8 @@ public class TrelloController {
     private TrelloClient trelloClient;
 
     @RequestMapping(method = RequestMethod.GET, value = "getTrelloBoards")
-    public void getTrelloBoards() {
-        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
+    public List<TrelloBoardDto> getTrelloBoards() {
+        /*List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
         trelloBoards.stream()
                 .filter(trelloBoardDto -> trelloBoardDto.getId()!= null)
                 .filter(trelloBoardDto -> trelloBoardDto.getName().contains("Kodilla"))
@@ -30,7 +30,10 @@ public class TrelloController {
                     System.out.println("This board contains lists: ");
                     trelloBoardDto.getLists().forEach(trelloList ->
                             System.out.println(trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed()));
-                });
+                });*/
+        //comment left for educational purpose
+
+        return trelloClient.getTrelloBoards();
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "createTrelloCard")
