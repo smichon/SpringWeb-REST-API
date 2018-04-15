d2a47745acc70b270e7b56fe4f95dd34fbaf3fd9
smichon <sylwester.michon@gmail.com>
2018-04-15 21:03:00 +0200

    mod 19.4 @Scheduled annotation added to mail message

---
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -0,0 +1,32 @@
+package com.crud.tasks.scheduler;
+
+import com.crud.tasks.domain.Mail;
+import com.crud.tasks.repository.TaskRepository;
+import com.crud.tasks.service.SimpleEmailService;
+import com.crud.tasks.trello.config.AdminConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailScheduler {
+    @Autowired
+    private SimpleEmailService simpleEmailService;
+    @Autowired
+    private TaskRepository taskRepository;
+    @Autowired
+    private AdminConfig adminConfig;
+    private static final String SUBJECT = "Tasks: Once a day email";
+
+    @Scheduled(cron = "0 0 10 * * *")
+//    @Scheduled(fixedDelay = 10000)
+    public void sendInformationEmail() {
+        long size = taskRepository.count();
+        String tasks = size != 1 ? "tasks" : "task";
+        simpleEmailService.send(new Mail(
+                adminConfig.getAdminMail(),
+                null,
+                SUBJECT,
+                "Currently in database you got: " + size + " " + tasks));
+    }
+}
